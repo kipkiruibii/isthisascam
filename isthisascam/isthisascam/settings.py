66361c3291be22d7861d02b8eedcab9e09b18799
kipkiruibii <biihezron1@gmail.com>
2024-09-14 20:00:09 +0300

    project

--- isthisascam/isthisascam/settings.py
+++ isthisascam/isthisascam/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'isthisascam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config.get('POSTGRES_DATABASE'),
+            'USER': config.get('POSTGRES_USER'),
+            'PASSWORD': config.get('POSTGRES_PASSWORD'),
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
