3a1f9fb22199caf082a0e01041efc94a9dc3af7a
kipkiruibii <biihezron1@gmail.com>
2024-09-15 20:30:38 +0300

    all elements done

--- isthisascam/isthisascam/settings.py
+++ isthisascam/isthisascam/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ISLOCAL = True
+ISLOCAL = False
 DEBUG = False
 if ISLOCAL:
     DEBUG = True
