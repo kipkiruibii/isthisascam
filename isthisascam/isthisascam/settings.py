ff1403d7201b45e6cfc2a38ea3afb6ef8dc104cd
kipkiruibii <biihezron1@gmail.com>
2024-09-15 14:52:09 +0300

    all elements done

--- isthisascam/isthisascam/settings.py
+++ isthisascam/isthisascam/settings.py
@@ -29,7 +29,7 @@ else:
 SECRET_KEY = config.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['3.15.224.229', '127.0.0.1', '192.168.100.54']
 
